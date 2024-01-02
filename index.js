c43f2e8080d5ad598565b601161a149ab0b53ee1
bale <lxbach1608@gmail.com>
2024-01-02 15:04:36 +0700

    npm nodemon, morgan

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+var morgan = require("morgan");
 const app = express();
 const port = 3005;
+
+app.use(morgan("combined"));
 
 app.get("/main", (req, res) => {
   res.send("Hello World!");
