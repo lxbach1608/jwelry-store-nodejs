721547ca6c66abe874936e19e63fa92f5a8b9799
bale <lxbach1608@gmail.com>
2024-01-02 15:28:00 +0700

    template engine handlebars

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+var morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3005;
+
+app.use(morgan("combined"));
+
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
